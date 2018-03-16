862e1f7fc3082fe620c4b33d52757ccbc8bbca33
danqing.zhang <danqing.zhang@yingzt.com>
2018-03-16 08:43:57 +0800

    修改图

--- page/index/index.js
+++ page/index/index.js
@@ -12,9 +12,8 @@ let pageObject = {
             isShow: false
         },
         imgUrls: [
-            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+            '../../img/slide1.png',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'
         ],
         indicatorDots: true,
         autoplay: true,
